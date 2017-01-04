cb3fef2f231bad596711f0b2e70863a6137049b8
Frank Groenewoud <frank@groenewoud.me>
2017-01-04 14:51:49 +0100

    mv to cp

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 mkdir -p /config/scripts
 
 mv -n /tmp/sickbeard_mp4_automator /config/scripts/
-mv -n /config/scripts/sickbeard_mp4_automator/autoProcess.ini.sample /config/scripts/sickbeard_mp4_automator/autoProcess.ini
+cp /config/scripts/sickbeard_mp4_automator/autoProcess.ini.sample /config/scripts/sickbeard_mp4_automator/autoProcess.ini
 
 mkdir -p /config/cache
 
